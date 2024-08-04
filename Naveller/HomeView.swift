b59204fdb3ee8ecb0d6e6cbfe9abc34e8a9f25a1
Paul <paulpopa612@gmail.com>
2024-08-04 01:45:48 +0100

    [Clean] Add HomeView file

---
+++ Naveller/HomeView.swift
@@ -0,0 +1,14 @@
+//
+//  HomeView.swift
+//  Naveller
+//
+//  Created by Paul on 04/08/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+       Text("Home")
+    }
+}
