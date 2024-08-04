134df14328e4ae52e11f788eae4ad0f43ec73c35
Paul <paulpopa612@gmail.com>
2024-08-04 23:55:50 +0100

    [Feature] Create Pin Entity defining pin type & position on map

--- Domain/ShowPinsOnMap/Entities/Pin.swift
+++ Domain/ShowPinsOnMap/Entities/Pin.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+public struct Pin: Identifiable {
+    
+    public let id: String
+    let latitude: Double
+    let longitude: Double
+    let title: String
+    let description: String?
+    let icon: String?
+}
