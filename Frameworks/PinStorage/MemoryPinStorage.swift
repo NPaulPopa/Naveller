a4ee0826fa599426d710d8d561842f889732690d
Paul <paulpopa612@gmail.com>
2024-08-04 23:56:51 +0100

    [Feature] Create DataSource implementing PinDataRepository reporitory to return mock Pin entities

--- Frameworks/PinStorage/MemoryPinStorage.swift
+++ Frameworks/PinStorage/MemoryPinStorage.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+public class MemoryPinStorageDataSource: PinDataRepository {
+    
+    let pins = [
+        
+        Pin(id: "1", latitude: 1.1, longitude: 1.55, title: "Cafe W", description: "coffe shop", icon: "https://icon.me"),
+        Pin(id: "2", latitude: 2.1, longitude: 2.55, title: "Italian Bistro", description: "italian", icon: "https://icon.me"),
+    ]
+    
+    public func fetchAllPins() async -> [Pin] {
+       // try! await Task.sleep(nanoseconds: 30_000_000)
+        return pins
+    }
+}
