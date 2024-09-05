01820bb1d7a0609425e07d026d02a0fbdc2d9ddf
Paul <paulpopa612@gmail.com>
2024-09-05 23:57:49 +0100

    Create PinsViewModel

--- Features/ShowPinsOnMap/Presentation/ViewModel/PinsViewModel.swift
+++ Features/ShowPinsOnMap/Presentation/ViewModel/PinsViewModel.swift
@@ -11,20 +11,5 @@ class PinsViewModel: ObservableObject {
     
   var pins: [Pin] = []
     
-    let pinsRepository: PinDataRepository
-    
-    init(pinsRepository: PinDataRepository) {
-        self.pinsRepository = pinsRepository
-        
-        fetchPins()
-    }
-    
-    //Methods
-    
-    public func fetchPins() {
-       
-        Task {
-            await pins = pinsRepository.fetchAllPins()
-        }
-    }
+
 }
